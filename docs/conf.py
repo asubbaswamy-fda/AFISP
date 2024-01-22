bad98ef1a8fa2326ab2d07bd8b8866c08c0ceae2
asubbaswamy-fda <adarsh.subbaswamy@fda.hhs.gov>
2024-01-22 03:43:42 -0500

    Adding mock import for RTD

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath(".."))
-autodoc_mock_imports = ["numpy", "sklearn"]
+autodoc_mock_imports = ["numpy", "sklearn", "tqdm"]
 
 project = 'AFISP'
 copyright = '2024, Adarsh Subbaswamy'
